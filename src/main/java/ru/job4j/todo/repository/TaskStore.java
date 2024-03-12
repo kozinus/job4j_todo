f1bc164f5ad18a3c199c8bfaed01ba3e39758734
kozinus <kkozin2403@gmail.com>
2024-03-12 03:05:22 +0300

    6. Лямбды и шаблон Command [#49295]

--- src/main/java/ru/job4j/todo/repository/TaskStore.java
+++ src/main/java/ru/job4j/todo/repository/TaskStore.java
@@ -1,150 +1,71 @@
 package ru.job4j.todo.repository;
 
 import lombok.AllArgsConstructor;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Repository;
 import ru.job4j.todo.model.Task;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Repository
 @AllArgsConstructor
 public class TaskStore {
-    private final SessionFactory sf;
+    private final CrudStore crudStore;
 
     public Task save(Task task) {
-        Session session = sf.openSession();
-        try {
-            session.beginTransaction();
-            session.save(task);
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        } finally {
-            session.close();
-        }
+        crudStore.run(session -> session.persist(task));
         return task;
     }
 
     public boolean update(Task task) {
-        Session session = sf.openSession();
-        boolean flag = false;
+        boolean result;
         try {
-            session.beginTransaction();
-            session.update(task);
-            session.getTransaction().commit();
-            flag = true;
+            crudStore.run(session -> session.merge(task));
+            result = true;
         } catch (Exception e) {
-            session.getTransaction().rollback();
-        } finally {
-            session.close();
+            result = false;
         }
-        return flag;
+        return result;
     }
 
-    public boolean delete(int id) {
-        Session session = sf.openSession();
-        boolean flag = false;
-        try {
-            session.beginTransaction();
-            flag = session.createQuery("delete Task where id = :Id")
-                    .setParameter("Id", id).executeUpdate() > 0;
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        } finally {
-            session.close();
-        }
-        return flag;
+    public void delete(int id) {
+        crudStore.run(
+                "delete from Task where id = :fId",
+                Map.of("fId", id)
+        );
     }
 
     public boolean makeDone(int id) {
-        Session session = sf.openSession();
-        boolean flag = false;
+        boolean result;
         try {
-            session.beginTransaction();
-            session.createQuery("update Task set done = true where id = :Id")
-                    .setParameter("Id", id)
-                    .executeUpdate();
-            session.getTransaction().commit();
-            flag = true;
+            crudStore.run(
+                    "update Task set done = true where id = :fId",
+                    Map.of("fId", id)
+            );
+            result = true;
         } catch (Exception e) {
-            session.getTransaction().rollback();
-        } finally {
-            session.close();
+            result = false;
         }
-        return flag;
+        return result;
     }
 
     public List<Task> findAll() {
-        Session session = sf.openSession();
-        List<Task> result = new ArrayList<>();
-        try {
-            session.beginTransaction();
-            result = session.createQuery(
-                    "from Task order by id asc", Task.class).list();
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        } finally {
-            session.close();
-        }
-        return result;
+        return crudStore.query("from Task order by id asc", Task.class);
     }
 
     public List<Task> findAllDone() {
-        Session session = sf.openSession();
-        List<Task> result = new ArrayList<>();
-        try {
-            session.beginTransaction();
-            result = session.createQuery(
-                    "from Task where done = true order by id asc", Task.class).list();
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        } finally {
-            session.close();
-        }
-        return result;
+        return crudStore.query("from Task where done = true order by id asc", Task.class);
     }
 
     public List<Task> findAllNew() {
-        Session session = sf.openSession();
-        List<Task> result = new ArrayList<>();
-        try {
-            session.beginTransaction();
-            result = session.createQuery(
-                    "from Task where created > :now order by id asc", Task.class)
-                    .setParameter("now", LocalDateTime.now().withNano(0).withSecond(0).minusMinutes(3))
-                    .list();
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        } finally {
-            session.close();
-        }
-        return result;
+        return crudStore.query("from Task where created > :now order by id asc", Task.class,
+                Map.of("now", LocalDateTime.now().withNano(0).withSecond(0).minusMinutes(3)));
     }
 
     public Optional<Task> findById(int id) {
-        Session session = sf.openSession();
-        Optional<Task> result = Optional.empty();
-        try {
-            session.beginTransaction();
-            result = Optional.of(session.createQuery(
-                            "from Task where id = :Id", Task.class)
-                    .setParameter("Id", id)
-                    .getSingleResult());
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        } finally {
-            session.close();
-        }
-        return result;
+        return crudStore.optional("from Task where id = :Id", Task.class,
+                Map.of("Id", id));
     }
 }
